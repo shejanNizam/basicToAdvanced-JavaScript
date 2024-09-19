6d65eef81318ad72836df4599595d4069214fda3
shejanNizam <nizam.shejan11@gmail.com>
2024-09-19 16:33:49 +0600

    variable practice

---
+++ basic-fundamentals/1.variable/practice.js
@@ -0,0 +1,39 @@
+//1.
+let totalMoney = 1000;
+let applePrice = 400;
+let orangePrice = 300;
+let totalBuy = applePrice + orangePrice;
+let returnMoney = totalMoney - totalBuy;
+console.log(returnMoney);
+
+//
+
+//2.
+let math = 75.25;
+let bio = 65;
+let phy = 80;
+let eng = 35.45;
+let bang = 99.5;
+
+let totalMarks = math + bio + phy + eng + bang;
+let avrage = totalMarks / 5;
+let avgInt = Number(avrage.toFixed(4));
+
+console.log(avgInt);
+
+//
+
+//3.
+let num1 = 119;
+let num2 = 5;
+let remainder = num1 % num2;
+console.log(remainder);
+
+//
+
+//4.
+const a = isNaN("11");
+const b = isNaN(2 - 10);
+console.log(b);
+
+//
