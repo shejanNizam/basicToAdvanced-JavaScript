7e8e80b999e34a673fd03d164f80abdfeb704ba2
shejanNizam <nizam.shejan11@gmail.com>
2024-10-02 16:35:36 +0600

    ternary operator

---
+++ javaScript-refresher/8.truthyFalsy.js
@@ -0,0 +1,4 @@
+// In whole JavaScript
+
+falsy values --> false, 0, undefined, null, "", NaN
+truthy values --> rest of all are truthy
---
+++ javaScript-refresher/9.ternaryOperator.js
@@ -0,0 +1,4 @@
+// Ternary Operator --> for max two layer
+
+const a = 15;
+const result = a % 2 === 0 ? "Even" : a % 3 === 0 ? "Div by 3" : "Sim Odd";
