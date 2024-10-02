b4682ae0df574286af4d9613e4e00e6fdc057b8b
shejanNizam <nizam.shejan11@gmail.com>
2024-10-02 19:15:04 +0600

    optional chaining & nullish qualescing operator

--- javaScript-refresher/12.nullishQualescing.js
+++ javaScript-refresher/12.nullishQualescing.js
@@ -1,3 +1,7 @@
 // Nullish Qualescing Operator
 
 //  Nullish --> null / undefined
+const lang = null;
+const qualescing = lang ?? "JS"; // if lang = any nullish value return "JS"
+const logicalOr = lang || "JS"; // if lang = any falsy value return "JS"
+const logicalAnd = lang && "JS"; // if lang = any truthy value return "JS"
--- script.js
+++ script.js
@@ -3,7 +3,3 @@
 // let result = first + " " + second;
 
 // console.log(result);
-const lang = null;
-const qualescing = lang ?? "JS"; // if lang = any nullish value return "JS"
-const logicalOr = lang || "JS"; // if lang = any falsy value return "JS"
-const logicalAnd = lang && "JS"; // if lang = any truthy value return "JS"
