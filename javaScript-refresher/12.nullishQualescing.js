5c74c9e1c08baefde4dc2f283dc46b40a75917ff
shejanNizam <nizam.shejan11@gmail.com>
2024-10-02 19:37:49 +0600

    asynchoronous JavaScript

--- javaScript-refresher/12.nullishQualescing.js
+++ javaScript-refresher/12.nullishQualescing.js
@@ -5,3 +5,5 @@ const lang = null;
 const qualescing = lang ?? "JS"; // if lang = any nullish value return "JS"
 const logicalOr = lang || "JS"; // if lang = any falsy value return "JS"
 const logicalAnd = lang && "JS"; // if lang = any truthy value return "JS"
+
+// they are called SHORT  CIRCUIT CONDITIONALS
---
+++ javaScript-refresher/13.asynchronousJS.js
@@ -0,0 +1,4 @@
+function getData() {
+  console.log("Asynchoronous JavaScript");
+}
+getData();
