d898cc1bc38fb46a8971eca3a02c2affe4629aa9
shejanNizam <nizam.shejan11@gmail.com>
2024-10-23 20:43:25 +0600

    array redue method

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 // let result = first + " " + second;
 
 // console.log(result);
+
+const numbers = [1, 2, 3, 4];
+
+const sum = numbers.reduce((total, currValue) => total + currValue);
+
+console.log(sum);
