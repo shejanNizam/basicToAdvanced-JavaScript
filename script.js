c3df58ac45f4e59246ebc639632f876830c3ce88
nizam <nizam.shejan11@gmail.com>
2025-07-13 18:45:14 +0600

    arry

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ let first = "I Learn";
 let second = "JS";
 let result = first + " " + second;
 
-console.log(result);
+// console.log(result);
+
+const myArr = [1, 2, 3, 4, 5];
+
+const newArr = myArr.push(5);
+console.log(newArr);
