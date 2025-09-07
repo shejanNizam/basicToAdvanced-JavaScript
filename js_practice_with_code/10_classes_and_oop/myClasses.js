a5b10dd44414d887d0a0f0a0091e29a079fbac8f
nizam <nizam.shejan11@gmail.com>
2025-09-07 11:36:27 +0600

    create user using normal func

--- js_practice_with_code/10_classes_and_oop/myClasses.js
+++ js_practice_with_code/10_classes_and_oop/myClasses.js
@@ -1,3 +1,20 @@
+function User(username, email, password) {
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
+
+User.prototype.encryptedPassword = function () {
+  return `${this.password}123`;
+};
+User.prototype.changeUsername = function () {
+  return `${this.username.toUpperCase()}`;
+};
+
+const user1 = new User("john_doe", "john@example.com", "password");
+console.log(user1.encryptedPassword());
+console.log(user1.changeUsername());
+
 class User {
   constructor(username, email, password) {
     this.username = username;
@@ -14,7 +31,7 @@ class User {
   }
 }
 
-const user1 = new User("john_doe", "john@example.com", "password");
+const user2 = new User("john_doe", "john@example.com", "password");
 
-console.log(user1.encryptedPassword());
-console.log(user1.changeUsername());
+console.log(user2.encryptedPassword());
+console.log(user2.changeUsername());
